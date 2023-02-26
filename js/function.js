96d336437d6a7f0f7914ebcf7f9051599a19287c
sib1979 <rinaldi.beltramone@gmail.com>
2023-02-26 14:45:06 -0300

    Se agrega el Proyecto

---
+++ js/function.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_color').addEventListener('click', function () { 
+    document.body.style.backgroundColor = '#EA899A';
+});
+document.getElementById('boton_default').addEventListener('click', function () { 
+    document.body.style.backgroundColor = '#09f0f8';
+});
+
